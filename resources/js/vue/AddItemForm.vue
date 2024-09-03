f5651b633bd46760478dea8991a2b86695a34405
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-09-03 23:40:00 +0500

    Add Item Modal added

--- resources/js/vue/AddItemForm.vue
+++ resources/js/vue/AddItemForm.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="addItem">
-    <input class="inputfield" type="text" :placeholder="$t('placeholder')" v-model="item.name">
-    <font-awesome-icon
-      icon="file-circle-plus"
-      @click="addItem"
-      :class="[item.name ? 'active' : 'inactive', 'plus']"
-    />
+  <div>
+    <div class="addItem">
+      <button @click="openAddItemModal" class="plus">
+        <font-awesome-icon icon="file-circle-plus" />
+      </button>
+    </div>
+    <!-- Add Item Modal -->
+    <div v-if="addModal" class="add-modal-overlay">
+      <div class="add-modal-content">
+        <h3 class="add-item-title">{{ $t('addItemTitle') }}</h3>
+        <input class="inputfield" type="text" :placeholder="$t('placeholder')" v-model="item.name">
+        <div class="add-buttons">
+          <button class="add-button" @click="addItem">{{ $t('AddItem') }}</button>
+          <button class="cancel-add-button" @click="closeAddItemModal">{{ $t('CancelAddItem') }}</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +25,7 @@ import { mapActions } from 'vuex';
 export default {
   data() {
     return {
+      addModal: false,
       item: {
         name: ""
       }
@@ -22,17 +33,23 @@ export default {
   },
   methods: {
     ...mapActions(['createItem']),
+    openAddItemModal() {
+      this.addModal = true;
+    },
+    closeAddItemModal() {
+      this.addModal = false;
+    },
     async addItem() {
-        if (this.item.name === "") return;
-        try {
-            await this.createItem(this.item);
-            this.item.name = "";
-            this.$emit('reloadList');
-        } catch (error) {
-            console.error('Error adding item:', error);
-        }
+      if (this.item.name.trim() === "") return;
+      try {
+        await this.createItem(this.item);
+        this.item.name = "";
+        this.$emit('reloadList');
+        this.closeAddItemModal();
+      } catch (error) {
+        console.error('Error adding item:', error);
+      }
     }
-}
+  }
 };
 </script>
-
