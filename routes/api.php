4a1eec9100aec493118f85e213ca6ad3bbfe2d50
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-09-04 21:54:22 +0500

    Made some changes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'getUser'])->middleware('auth:sanctum');
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/user', [UserController::class, 'user'])->middleware('auth:sanctum');
+
 
 Route::get('/items', [ItemController::class, 'index']);
 Route::prefix('/item')->group(function () {
