64d3d0e1695cb8e53496ce885c9d87a5ef6882db
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-09-20 02:22:32 +0500

    made some changes

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,39 +1,16 @@
 <template>
-  <div class="todoListContainer">
-    <div class="heading">
-      <locale-switcher id="lan"/>
-      <h1 id="title">{{ $t('todoList') }}</h1>
-      <add-item-form v-on:reloadList="fetchItems" />
-      <upload-excel-file />
-      <export-excel-file />
-
-    </div>
-    <list-view v-on:reloadList="fetchItems" />
+  <div>
+      <todo-page />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-import ListView from './ListView.vue';
-import AddItemForm from './AddItemForm.vue'; 
-import LocaleSwitcher from './LocaleSwitcher.vue';
-import UploadExcelFile from './UploadExcelFile.vue';
-import ExportExcelFile from './ExportExcelFile.vue';
+import TodoPage from './Todos/TodoPage.vue';
 
 export default {
   components: {
-    ListView,
-    AddItemForm,
-    LocaleSwitcher,
-    UploadExcelFile,
-    ExportExcelFile
+    TodoPage
   },
-  methods: {
-    ...mapActions(['fetchItems'])
-  },
-  created() {
-    this.fetchItems();
-  }
 };
 </script>
 
