33b3d13cdd62dcefe9761a2605d60b41047d6b41
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-09-25 14:11:25 +0500

    Vuex store refactored

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -50,87 +50,78 @@ const store = createStore({
     },
     actions: {
         // Fetch items from API and update store
-        fetchItems({ commit }) {
-            return axios.get(apiUrls.fetchItems)
-                .then(response => {
-                    commit('setItems', response.data); 
-                })
-                .catch(error => {
-                    console.error('Error fetching items:', error); 
-                });
+        async fetchItems({ commit }) {
+            try {
+                const response = await axios.get(apiUrls.fetchItems);
+                commit('setItems', response.data); 
+            } catch (error) {
+                console.error('Error fetching items:', error); 
+            }
         },
         // Add new item to store
-        createItem({ commit }, item) {
-            return axios.post(apiUrls.storeItem, { item })
-                .then(response => {
-                    commit('addItem', response.data); 
-                })
-                .catch(error => {
-                    console.error('Error adding item:', error); 
-                    throw error; 
-                });
+        async createItem({ commit }, item) {
+            try {
+                const response = await axios.post(apiUrls.storeItem, { item });
+                commit('addItem', response.data); 
+            } catch (error) {
+                console.error('Error adding item:', error); 
+            }
         },
         // Update existing item in store
-        updateItem({ commit }, item) {
-            return axios.put(apiUrls.updateItem(item.id), { item })
-                .then(response => {
-                    commit('updateItem', response.data); 
-                })
-                .catch(error => {
-                    console.error('Error updating item:', error); 
-                    throw error; 
-                });
+        async updateItem({ commit }, item) {
+            try {
+                const response = await axios.put(apiUrls.updateItem(item.id), { item });
+                commit('updateItem', response.data); 
+            } catch (error) {
+                console.error('Error updating item:', error); 
+            }
         },
         // Remove item from store
-        deleteItem({ commit }, itemId) {
-            return axios.delete(apiUrls.destroyItem(itemId))
-                .then(() => {
-                    commit('removeItem', itemId); 
-                })
-                .catch(error => {
-                    console.error('Error deleting item:', error); 
-                    throw error; 
+        async deleteItem({ commit }, itemId) {
+            try {
+                const response = await axios.delete(apiUrls.destroyItem(itemId));
+                commit('removeItem', itemId); 
+            } catch (error) {
+                console.error('Error deleting item:', error); 
+            }
+        },
+        // Upload excel file
+        async uploadExcelFile({ commit }, formData) {
+            try {
+                const respose = await axios.post(apiUrls.importItem, formData, {
+                    headers: {'Content-Type': 'multipart/form-data'}
                 });
-        },
-
-        uploadExcelFile({ commit }, formData) {
-            return axios.post(apiUrls.importItem, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-            .then(response => {
                 commit('setItems', response.data); 
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error('Error uploading Excel file:', error);
-                throw error;
-            });
+            }
         },
-        exportExcelFile() {
-            return axios({
-              url: apiUrls.exportItem,
-              method: 'GET',
-              responseType: 'blob'  
-            })
-            .then((response) => {
-              const url = window.URL.createObjectURL(new Blob([response.data]));
-              const link = document.createElement('a');
-              link.href = url;
-              link.setAttribute('download', 'items.xlsx');  
-              document.body.appendChild(link);
-              link.click();
-              link.remove();
-            })
-            .catch(error => {
-              console.error('Error exporting Excel file:', error);
-              throw error;
-            });
+        async exportExcelFile() {
+            try {
+                const response = await axios({
+                    url: apiUrls.exportItem,
+                    method: 'GET',
+                    responseType: 'blob'  
+                  });
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'items.xlsx');  
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error('Error exporting Excel file:', error);
+            }
           },
 
-        initDarkMode({ commit }) {
-            const savedMode = localStorage.getItem('dark-mode');
-            commit('setDarkMode', savedMode === 'true');
+        async initDarkMode({ commit }) {
+            try {
+                const savedMode = await localStorage.getItem('dark-mode');
+                commit('setDarkMode', savedMode === 'true');
+            } catch (error) {
+                console.error('Error initializing dark mode:', error);
+            }
         
         },
     },
